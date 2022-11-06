6227c99a5bd64e90fafc0e1223b9b3f02f3c5bb8
Zhitao Hou <zhith@microsoft.com>
2022-11-06 23:12:48 +0800

    Update react to 17.0.2

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,13 +1,10 @@
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
 import App from './app/app';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <FluentProvider theme={webLightTheme}>
     <App />
-  </FluentProvider>
+  </FluentProvider>,
+  document.getElementById('root')
 );
